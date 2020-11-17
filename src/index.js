f91b8c6bce8314fbb6e5ac36d6223e93b9ae02e4
Alex Sofronie <alsofronie@gmail.com>
2020-11-17 18:47:58 +0200

    refactor(initialize) Move initialize in usecases

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 // install the listeners
 import '@/lib/bus'
 
-import { initialize } from '@/config'
-import { showConnect, showProductCard, showProductDetails } from '@/usecases'
-
-export { initialize, showConnect, showProductCard, showProductDetails }
+export {
+  initialize,
+  showConnect,
+  showProductCard,
+  showProductDetails
+} from '@/usecases'

2f05cd3d9920e845bc259af35d3faa447e646b07
Alex Sofronie <alsofronie@gmail.com>
2021-04-21 15:12:26 +0300

    chore(package) Cleanup unused dependencies and update (audit)

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-echo "Uploading..."
-aws s3 sync dist s3://symbioticon/scripts/ --delete --exclude ".DS_Store" --profile etvas_symbioticon 
-echo "Invalidating..."
-aws cloudfront create-invalidation --distribution-id ELHD6RXAI459C --paths '/*' --profile etvas_symbioticon > /dev/null
-echo "Done."

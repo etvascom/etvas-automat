57fbb85c49e61eb47e5ce9e170f8310dc8b9ae15
Bogdan Onu <bogdan.onu@thinslices.com>
2021-06-28 11:42:55 +0300

    hotfix(cmdrc): Add prefix so that webpack correctly sets the variable

--- src/config/defaults.js
+++ src/config/defaults.js
@@ -1,5 +1,5 @@
 export const defaults = {
-  locale: process.env.DEFAULT_LOCALE || 'en',
+  locale: process.env.ETVAS_DEFAULT_LOCALE || 'en',
   etvasGraphQLUrl: process.env.ETVAS_GRAPHQL_URL,
   etvasGraphQLApiKey: process.env.ETVAS_GRAPHQL_API_KEY,
   'auth.region': process.env.ETVAS_REGION,
